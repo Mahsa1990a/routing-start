93b8239053beb8563e00a9ff39b0b5aa923539e9
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-01 11:25:31 -0400

    135. Fetching Route Parameters Reactively-with params(observable feature for working with async tasks)

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -21,6 +21,17 @@ export class UserComponent implements OnInit {
       id: this.routeProp.snapshot.params['idDynamic'], //idDynamic : the property you defined with route
       name: this.routeProp.snapshot.params['nameDynamic'] //any dynamic property in route would be retrivable here
     }
+
+    // to be able to reaction to subsequent changes(routerLink in temp) we need:
+    //params is observable, observables are feature(third party package) allow you easily work with async tasks
+    this.routeProp.params // to not block your code
+      .subscribe( //it will update user id & name whenever parameter change:
+        (updatedParams: Params) => {
+          this.user.id = updatedParams['idDynamic'];
+          this.user.name = updatedParams['nameDynamic'];
+        }
+      );
+
   }
 
 }
