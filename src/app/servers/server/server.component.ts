6a3a23226474682913f7928807babef75ca06dc3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-02 12:32:42 -0400

    139. Practicing and some Common Gotchas=> using id as number passing for updating server detail

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,24 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private routePre: ActivatedRoute
+    ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const getId = +this.routePre.snapshot.params['id']; //here id is not number 1 it's '1'(change it to number qith adding +)
+
+    // this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(getId);
+    // Now for reacting to any changes there after:
+    this.routePre.params
+      .subscribe(
+        (parameters: Params) => {
+          // get a new server whenever params change
+          this.server = this.serversService.getServer(+parameters['id']);//(change it to number qith adding +)
+        }
+      );
   }
 
 }
