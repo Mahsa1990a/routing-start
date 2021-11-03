1350be29175b0b2672605d44a41da857d1c62a9e
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-02 21:58:01 -0400

    145. Outsourcing the Route Configuration- create new file for all routes(app-routing.module.ts)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+  // path: after domin in url(localhost:4200/users), component: is action, which component should load
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+
+  { path: 'servers', component: ServersComponent, children: [ //adding child route, nested childs needs their own <router-outlet>
+    // { path: 'servers/:id', component: ServerComponent }, after adding as child:
+    { path: ':id', component: ServerComponent },
+    // { path: 'servers/:id/edit', component: EditServerComponent }, //after adding as child:
+    { path: ':id/edit', component: EditServerComponent }, //add new route
+    ]
+  },
+
+
+  { path: 'users', component: UsersComponent, children: [
+    // route for single user => : shows this idDynamic is dynamic
+    { path: ':idDynamic/:nameDynamic', component: UserComponent } //assign single UserComponent
+    // can add as many as dynamic property here ^ and retrive from component
+    ]
+  },
+
+  // for unkown routes:
+  {path: 'not-found', component: PageNotFoundComponent},
+  //           If don't want to specify components: use redirectTo: 'the path'(it redirect automatically to /not-found route)
+  {path: '**', redirectTo: '/not-found'} //should be the last one(route pars from top to bottom)
+  // for unkown routes
+];
+
+@NgModule({//decorator
+  imports: [
+    RouterModule.forRoot(appRoutes) // <= register routes into our app
+  ],
+  //want to outsource our routes(need to add AppRoutingModule to main app module):
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,33 +12,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  // path: after domin in url(localhost:4200/users), component: is action, which component should load
-  { path: '', component: HomeComponent },
-
-  { path: 'servers', component: ServersComponent, children: [ //adding child route, nested childs needs their own <router-outlet>
-    // { path: 'servers/:id', component: ServerComponent }, after adding as child:
-    { path: ':id', component: ServerComponent },
-    // { path: 'servers/:id/edit', component: EditServerComponent }, //after adding as child:
-    { path: ':id/edit', component: EditServerComponent }, //add new route
-    ]
-  },
-
-
-  { path: 'users', component: UsersComponent, children: [
-    // route for single user => : shows this idDynamic is dynamic
-    { path: ':idDynamic/:nameDynamic', component: UserComponent } //assign single UserComponent
-    // can add as many as dynamic property here ^ and retrive from component
-    ]
-  },
-
-  // for unkown routes:
-  {path: 'not-found', component: PageNotFoundComponent},
-  //           If don't want to specify components: use redirectTo: 'the path'(it redirect automatically to /not-found route)
-  {path: '**', redirectTo: '/not-found'} //should be the last one(route pars from top to bottom)
-  // for unkown routes
-];
+// Move appRoutes into app-routing.module.ts
 
 @NgModule({
   declarations: [
@@ -54,8 +30,10 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    // gister routes into our app:
-    RouterModule.forRoot(appRoutes)
+    // register routes into our app:
+    // RouterModule.forRoot(appRoutes) //move into app-routing.module.ts
+
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
